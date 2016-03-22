4aa35a884f941820e0359f2ed80c161baa950330
scc <1437366608@qq.com>
2016-03-22 19:32:29 +0800

    kua yu

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(logger('dev'));
 //解析body传输的内容
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//支持跨域
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
 
 //路由
 app.use('/file',files);
