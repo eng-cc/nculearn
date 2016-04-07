990712084b766bf42cf43eaf101e2c8cf94b5720
scc <1437366608@qq.com>
2016-04-07 15:01:45 +0800

    as api

--- routers/user.js
+++ routers/user.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.post('/login', function(req, res) {
     res.json({
         succ: true,
-        msg: {
+        msg: [
             {
                 title: tit1,
                 pictureurl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3117925159,1877649339&fm=80',
@@ -22,7 +22,7 @@ router.post('/login', function(req, res) {
                 pictureurl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3117925159,1877649339&fm=80',
                 article: '2014年10月，陆军第21集团军某特战旅中尉排长陈明珠等15名从全军选拔出来的优秀青年军官由总部派遣，成为委内瑞拉国民警卫队特种作战学校(Grupo de Acciones de Comando)招收的首批中国籍军事留学生。'
             }
-        }
+        ]
     })
 });
 
